dfd83965301560119e0ed77a0eb05e4c4fb52066
moenn1 <mohamed.enn2001@gmail.com>
2023-04-03 04:31:35 +0000

    rf

--- src/CodeSignal/Rounders.java
+++ src/CodeSignal/Rounders.java
@@ -1,8 +1,17 @@
 package CodeSignal;
 
 public class Rounders {
-    int solution(int n) {
+    static int solution(int n) {
+        int i = 1;
+        while(n>10){
+            i *= 10;
+            n = n/10 + ((n%10 >= 5) ? 1: 0);
+        }
+        return n*i;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(solution(1234));
     }
 
 }
